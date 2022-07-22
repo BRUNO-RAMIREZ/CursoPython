b90db7c396e39b5ed09fdadf08a03b82b1b31de7
BRUNO-RAMIREZ <89104842+BRUNO-RAMIREZ@users.noreply.github.com>
2022-07-21 21:11:59 -0400

    Add files via upload

---
+++ 2-AsignacionValores.py
@@ -0,0 +1,28 @@
+entero = 20;
+print(entero);
+print(type(entero))
+
+decimal = 10.50;
+print(decimal);
+print(type(decimal));
+
+cadena = 'Programacion';
+cadena2 = "Competitiva";
+cadena3 = "Aprendiendo 'python'";
+print(cadena3);
+print(type(cadena3));
+
+booleano = True;
+print(booleano);
+print(type(booleano));
+
+num1 = 20;
+num2 = 15;
+resultado = num1 + num2 * 10 / 5;
+print("El resultado es ",resultado);
+
+#Tipado dinamico una misma variable puede tomar o contener diferentes tipos de datos
+variable = 20;
+print(variable);
+variable = "Hola mundo";
+print(variable);
